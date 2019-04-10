6d08cceac1e0614b0b5d552ebbe297c8e334e360
sabrinaroldan33 <47401642+sabrinaroldan33@users.noreply.github.com>
2019-04-09 22:32:45 -0300

    Add files via upload

--- clase06gets/main.c
+++ clase06gets/main.c
@@ -34,8 +34,22 @@ int getNumber(  char *msg, char *msgError,int minimo, int maximo, int reintentos
                 int *resultado);
 
     int retorno=-1;
-
+    char buffer(18)
+    int bufferInt;
     if msh!=null //validar
+    (
+        if ( utn_getString(msg,msgError,1,16,reintentos,buffer)==0)
+        (
+          if(utn_isValidNumber(buffer))
+          (
+          bufferInt=atoi(buffer);
+          if(bufferInt>=minimo && bufferInt<=maximo)
+          (
+                *resultado=bufferINt;
+                retorno =0;
+          )
+
+        )
 
     //recibir los parametros bien y obtener el numero que esta ingresando el usuario
     punteros no null
