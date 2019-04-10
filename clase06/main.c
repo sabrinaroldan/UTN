380ee25f95ae2f2dd2776fd46ece0195c0a4af1d
sabrinaroldan33 <47401642+sabrinaroldan33@users.noreply.github.com>
2019-04-09 22:14:14 -0300

    Add files via upload

--- clase06/main.c
+++ clase06/main.c
@@ -1,35 +1,59 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#define CANTIDAD_CARACTERES 200
 /* [] ||
 --{}  */
 int main()
 {
-    char nombre[257];
-    char buffer[257];
+    char nombre[CANTIDAD_CARACTERES];
+    char buffer[CANTIDAD_CARACTERES];
     int i;
     int error=-1;
 
     printf("\nIngrese su nombre\n");
     fgets(buffer,sizeof(buffer),stdin);
 
+    buffer[strlen(buffer)-1]='\0';
+
     for(i=0;i<=sizeof(buffer);i++)
     {
         buffer[i];
-
-        if(buffer[i]=='*'||buffer[i]=='1'||buffer[i]=='2'||buffer[i]=='3'
-        ||buffer[i]=='4'||buffer[i]=='5'||buffer[i]=='6'||buffer[i]=='7'
-        ||buffer[i]=='8'||buffer[i]=='9'||buffer[i]=='0'||buffer[i]=='+'
-        ||buffer[i]=='-')
+        if(buffer[i]<'a' || buffer[i]>'z' && buffer[i]<'A' || buffer[i]>'Z')
         {
             printf("\nUd ha ingresado un caracter no valido\n");
             return error;
         }
+
         nombre[i]=buffer[i];
     }
 
-    strncpy(buffer,nombre,sizeof(buffer));
+    strncpy(nombre,buffer,strlen(buffer));
     printf("\nEl nombre ingresado es: %s", nombre);
 
     return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ clase06gets/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+//{}
+int utn_isValidNumber(char *str);
+int getNumber(  char *msg, char *msgError,int minimo, int maximo, int reintentos,
+                int resultado);
+
+int getString(  char *msg, char *msgError,int minimo, int maximo, int reintentos,
+                char resultado);
+
+int main()
+{
+
+
+    return 0;
+}
+
+int utn_isValidNumber(char*str)
+{
+
+    return 1;
+
+
+}
